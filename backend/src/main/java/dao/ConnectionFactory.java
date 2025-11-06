70973c6651907a4acf1e46c4ca03644566e8f296
Camila <contatocamilamenezes@gmail.com>
2025-11-06 07:32:58 -0300

    aaaa

--- backend/src/main/java/dao/ConnectionFactory.java
+++ backend/src/main/java/dao/ConnectionFactory.java
@@ -7,7 +7,7 @@ public class ConnectionFactory {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/grwm";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "PostGTi2cc*";
+    private static final String PASSWORD = "1234";
 
     public static Connection getConnection() {
         try {
